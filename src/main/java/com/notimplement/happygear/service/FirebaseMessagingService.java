64954f08f0463b6c3b40157674c0097c09ba17f8
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-06-24 11:46:37 +0700

    update push notification

---
+++ src/main/java/com/notimplement/happygear/service/FirebaseMessagingService.java
@@ -0,0 +1,35 @@
+package com.notimplement.happygear.service;
+
+import org.springframework.stereotype.Service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import com.notimplement.happygear.model.dto.Note;
+
+@Service
+public class FirebaseMessagingService {
+    private final FirebaseMessaging firebaseMessaging;
+
+    public FirebaseMessagingService(FirebaseMessaging firebaseMessaging) {
+        this.firebaseMessaging = firebaseMessaging;
+    }
+
+    public String sendNotification(Note note, String token) throws FirebaseMessagingException {
+        Notification notification = Notification
+                .builder()
+                .setTitle(note.getSubject())
+                .setBody(note.getContent())
+                .build();
+
+        Message message = Message
+                .builder()
+                .setToken(token)
+                .setNotification(notification)
+                .putAllData(note.getData())
+                .build();
+
+        return firebaseMessaging.send(message);
+    }
+}
