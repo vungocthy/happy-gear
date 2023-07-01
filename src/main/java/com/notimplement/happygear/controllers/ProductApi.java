5541293b3b9c968b4daa06829b04aaac38a0df46
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-07-02 00:22:18 +0700

    update api product and filter

--- src/main/java/com/notimplement/happygear/controllers/ProductApi.java
+++ src/main/java/com/notimplement/happygear/controllers/ProductApi.java
@@ -21,129 +21,149 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ProductApi {
 
-	private final ProductService productService;
-	private final ProductPictureService productPictureService;
+    private final ProductService productService;
+    private final ProductPictureService productPictureService;
 
-	// @GetMapping("")
-	// public ResponseEntity<?> listAllProduct(){
-	// Pageable pageable = PageRequest.of(0,6);
-	// Map<List<ProductDto>, Integer> listIntegerMap =
-	// productService.listByPage(pageable);
-	// List<Object> list = new ArrayList<>();
-	// listIntegerMap.forEach((productDtos, integer) -> {
-	// list.add(productDtos);
-	// list.add(integer);
-	// });
-	// return ResponseEntity.ok(list);
-	// }
+    @GetMapping("/best-selling")
+    public ResponseEntity<?> listBestSellingProduct() {
+        return ResponseEntity.ok(productService.listAllBestSellingProduct());
+    }
 
-	@GetMapping("/best-selling")
-	public ResponseEntity<?> listBestSellingProduct() {
-		return ResponseEntity.ok(productService.listAllBestSellingProduct());
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<?> productById(@PathVariable("id") Integer id) {
+        var results = productService.getById(id);
+        return ResponseEntity.ok(results);
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<?> productById(@PathVariable("id") Integer id) {
-		var results = productService.getById(id);
-		return ResponseEntity.ok(results);
-	}
+    // @GetMapping("")
+    // public ResponseEntity<?> listProductByPage(@RequestParam("page")
+    // Optional<Integer> page,
+    // @RequestParam("limit") Optional<Integer> limit) {
 
-	// @GetMapping("/total")
-	// public ResponseEntity<?> totalProduct() {
-	// 	return ResponseEntity.ok(productService.totalProduct());
-	// }
+    // if (page.get() > 0) {
+    // page = Optional.of(page.get() - 1);
+    // } else if (page.get() < 0) {
+    // page = Optional.of(0);
+    // }
 
-	@GetMapping("")
-	public ResponseEntity<?> listProductByPage(@RequestParam("page") Optional<Integer> page,
-			@RequestParam("limit") Optional<Integer> limit) {
+    // Pageable pageable = PageRequest.of(page.orElse(0), limit.orElse(6));
+    // Map<List<ProductDto>, Integer> listIntegerMap =
+    // productService.listByPage(pageable);
 
-		if (page.get() > 0) {
-			page = Optional.of(page.get() - 1);
-		} else if (page.get() < 0) {
-			page = Optional.of(0);
-		}
+    // PaginationObject paginationObject = new PaginationObject();
 
-		Pageable pageable = PageRequest.of(page.orElse(0), limit.orElse(6));
-		Map<List<ProductDto>, Integer> listIntegerMap = productService.listByPage(pageable);
+    // listIntegerMap.forEach((productDtos, integer) -> {
+    // paginationObject.setData(productDtos);
+    // paginationObject.setSize(integer);
+    // });
+    // return ResponseEntity.ok(paginationObject);
+    // }
 
-		PaginationObject paginationObject = new PaginationObject();
+    @GetMapping("")
+    public ResponseEntity<?> listProductAndFilter(
+            @RequestParam("page") Optional<Integer> page,
+            @RequestParam("limit") Optional<Integer> limit,
+            @RequestParam("search") Optional<String> search,
+            @RequestParam("brandIds") Optional<List<Integer>> brandIds,
+            @RequestParam("categoryIds") Optional<List<Integer>> categoryIds,
+            @RequestParam("fromPrice") Optional<Double> fromPrice,
+            @RequestParam("toPrice") Optional<Double> toPrice,
+            @RequestParam("sortBy") Optional<String> sortBy) {
+        Map<List<ProductDto>, Integer> listIntegerMap = productService.listAllProductAndFilter(
+                page.orElse(0),
+                limit.orElse(8),
+                brandIds.orElse(null),
+                categoryIds.orElse(null),
+                fromPrice.orElse(0.0),
+                toPrice.orElse(Double.MAX_VALUE),
+                sortBy.orElse("asc"),
+                search.orElse("")
+        );
+        PaginationObject paginationObject = new PaginationObject();
+        listIntegerMap.forEach((productDtos, integer) -> {
+            paginationObject.setData(productDtos);
+            paginationObject.setSize(integer);
+        });
+        return ResponseEntity.ok(paginationObject);
+    }
 
-		listIntegerMap.forEach((productDtos, integer) -> {
-			paginationObject.setData(productDtos);
-			paginationObject.setSize(integer);
-		});
-		return ResponseEntity.ok(paginationObject);
-	}
+    @GetMapping("/{id}/pictures")
+    public ResponseEntity<?> listProductPictureByProductId(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(productPictureService.getByProductId(id));
+    }
 
-	@GetMapping("/{id}/pictures")
-	public ResponseEntity<?> listProductPictureByProductId(@PathVariable("id") Integer id) {
-		return ResponseEntity.ok(productPictureService.getByProductId(id));
-	}
+    @GetMapping("/latest")
+    public ResponseEntity<?> listLatestProduct() {
+        List<ProductDto> list = productService.listAllLatestProduct();
+        return ResponseEntity.ok(list);
+    }
 
-	// @GetMapping("/name")
-	// public ResponseEntity<?> listProductByPageAndName(@RequestParam("name") String name,
-	// 		@RequestParam("p") Optional<Integer> p) {
-	// 	Pageable pageable = PageRequest.of(p.orElse(0), 9);
-	// 	Map<List<ProductDto>, Long> listIntegerMap = productService.listByPageAndName(name, pageable);
-	// 	List<Object> list = new ArrayList<>();
-	// 	listIntegerMap.forEach((productDtos, integer) -> {
-	// 		list.add(productDtos);
-	// 		list.add(integer);
-	// 	});
-	// 	return ResponseEntity.ok(list);
-	// }
+    // @GetMapping("/name")
+    // public ResponseEntity<?> listProductByPageAndName(@RequestParam("name")
+    // String name,
+    // @RequestParam("p") Optional<Integer> p) {
+    // Pageable pageable = PageRequest.of(p.orElse(0), 9);
+    // Map<List<ProductDto>, Long> listIntegerMap =
+    // productService.listByPageAndName(name, pageable);
+    // List<Object> list = new ArrayList<>();
+    // listIntegerMap.forEach((productDtos, integer) -> {
+    // list.add(productDtos);
+    // list.add(integer);
+    // });
+    // return ResponseEntity.ok(list);
+    // }
 
-	// @GetMapping("/search")
-	// public ResponseEntity<?> listProductBySearch(@RequestParam("p") Optional<Integer> p,
-	// 		@RequestParam("text") Optional<String> text) {
-	// 	Pageable pageable = PageRequest.of(p.orElse(0), 9);
-	// 	Map<List<ProductDto>, Integer> listIntegerMap = productService.listProductByName(text.orElse(""), pageable);
-	// 	List<Object> list = new ArrayList<>();
-	// 	listIntegerMap.forEach((productDtos, integer) -> {
-	// 		list.add(productDtos);
-	// 		list.add(integer);
-	// 	});
-	// 	return ResponseEntity.ok(list);
-	// }
+    // @GetMapping("/search")
+    // public ResponseEntity<?> listProductBySearch(@RequestParam("p")
+    // Optional<Integer> p,
+    // @RequestParam("text") Optional<String> text) {
+    // Pageable pageable = PageRequest.of(p.orElse(0), 9);
+    // Map<List<ProductDto>, Integer> listIntegerMap =
+    // productService.listProductByName(text.orElse(""), pageable);
+    // List<Object> list = new ArrayList<>();
+    // listIntegerMap.forEach((productDtos, integer) -> {
+    // list.add(productDtos);
+    // list.add(integer);
+    // });
+    // return ResponseEntity.ok(list);
+    // }
 
-	// @GetMapping("/page")
-	// public ResponseEntity<?> listProductByPageAndCatgoryAndBrand(
-	// 		@RequestParam("p") Optional<Integer> p,
-	// 		@RequestParam("b") Optional<Integer> brandId,
-	// 		@RequestParam("c") Optional<Integer> categoryId,
-	// 		@RequestParam("f") Double fromPrice,
-	// 		@RequestParam("t") Double toPrice) {
-	// 	Pageable pageable = PageRequest.of(p.orElse(0), 9);
-	// 	Map<List<ProductDto>, Integer> listIntegerMap = productService.listByPageCategoryAndBrand(brandId.orElse(1),
-	// 			categoryId.orElse(1), fromPrice, toPrice, pageable);
-	// 	List<Object> list = new ArrayList<>();
-	// 	listIntegerMap.forEach((productDtos, integer) -> {
-	// 		list.add(productDtos);
-	// 		list.add(integer);
-	// 	});
-	// 	return ResponseEntity.ok(list);
-	// }
+    // @GetMapping("/page")
+    // public ResponseEntity<?> listProductByPageAndCatgoryAndBrand(
+    // @RequestParam("p") Optional<Integer> p,
+    // @RequestParam("b") Optional<Integer> brandId,
+    // @RequestParam("c") Optional<Integer> categoryId,
+    // @RequestParam("f") Double fromPrice,
+    // @RequestParam("t") Double toPrice) {
+    // Pageable pageable = PageRequest.of(p.orElse(0), 9);
+    // Map<List<ProductDto>, Integer> listIntegerMap =
+    // productService.listByPageCategoryAndBrand(brandId.orElse(1),
+    // categoryId.orElse(1), fromPrice, toPrice, pageable);
+    // List<Object> list = new ArrayList<>();
+    // listIntegerMap.forEach((productDtos, integer) -> {
+    // list.add(productDtos);
+    // list.add(integer);
+    // });
+    // return ResponseEntity.ok(list);
+    // }
 
-	@GetMapping("/latest")
-	public ResponseEntity<?> listLatestProduct() {
-		List<ProductDto> list = productService.listAllLatestProduct();
-		return ResponseEntity.ok(list);
-	}
+    // @PostMapping("/create")
+    // public ResponseEntity<?> createProduct(@Valid @RequestBody ProductDto
+    // Product) {
+    // return ResponseEntity.ok(productService.create(Product));
+    // }
 
-	// @PostMapping("/create")
-	// public ResponseEntity<?> createProduct(@Valid @RequestBody ProductDto Product) {
-	// 	return ResponseEntity.ok(productService.create(Product));
-	// }
+    // @PutMapping("/update/{id}")
+    // public ResponseEntity<?> updateProduct(@PathVariable(name = "id") Integer id,
+    // @Valid @RequestBody ProductDto p) {
+    // p.setProductId(id);
+    // return ResponseEntity.ok(productService.update(p));
+    // }
 
-	// @PutMapping("/update/{id}")
-	// public ResponseEntity<?> updateProduct(@PathVariable(name = "id") Integer id, @Valid @RequestBody ProductDto p) {
-	// 	p.setProductId(id);
-	// 	return ResponseEntity.ok(productService.update(p));
-	// }
-
-	// @DeleteMapping("/delete/{id}")
-	// public ResponseEntity<?> deleteProduct(@PathVariable(name = "id") Integer id) {
-	// 	return ResponseEntity.ok(productService.delete(id));
-	// }
+    // @DeleteMapping("/delete/{id}")
+    // public ResponseEntity<?> deleteProduct(@PathVariable(name = "id") Integer id)
+    // {
+    // return ResponseEntity.ok(productService.delete(id));
+    // }
 
 }
--- src/main/java/com/notimplement/happygear/repositories/ProductRepository.java
+++ src/main/java/com/notimplement/happygear/repositories/ProductRepository.java
@@ -10,21 +10,36 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer>{
+public interface ProductRepository extends JpaRepository<Product, Integer> {
     Product findByProductId(Integer id);
-    @Query("SELECT p FROM Product p WHERE p.brand.brandId = :brandId " +
-            "AND p.category.categoryId = :categoryId AND p.price between :fromPrice AND :toPrice")
+
+    @Query("SELECT p FROM Product p " +
+            "WHERE p.brand.brandId = :brandId " +
+            "AND p.category.categoryId = :categoryId " +
+            "AND p.price between :fromPrice AND :toPrice")
     Page<Product> findAllProductWithFilter(Integer brandId, Integer categoryId,
-                                        Double fromPrice, Double toPrice, Pageable pageable);
+                                           Double fromPrice, Double toPrice, Pageable pageable);
+
     List<Product> findTop4ByOrderByProductIdDesc();
+
     Page<Product> findByProductNameContainingIgnoreCase(String productName, Pageable pageable);
+
     Page<Product> findByProductNameContaining(String productName, Pageable pageable);
 
-    @Query(
-        "SELECT p FROM Product p WHERE p.productId IN (" +
-            "SELECT od.product.productId FROM OrderDetail od GROUP BY od.product.productId " +
-            "ORDER BY COUNT(od.product.productId) DESC " +
-        ")"
+    @Query("SELECT p FROM Product p " +
+            "WHERE p.productId IN (" +
+            "SELECT od.product.productId " +
+            "FROM OrderDetail od " +
+            "GROUP BY od.product.productId " +
+            "ORDER BY COUNT(od.product.productId) DESC )"
     )
     List<Product> findTop4BestSellingProduct(Pageable pageable);
+
+    @Query("SELECT p FROM Product p " +
+            "WHERE (:brandIds IS NULL OR p.brand.brandId IN (:brandIds))" +
+            "AND (:categoryIds IS NULL OR p.category.categoryId IN (:categoryIds)) " +
+            "AND p.price BETWEEN :fromPrice AND :toPrice " +
+            "AND p.productName LIKE %:search%")
+    Page<Product> findProductsAndFilter(List<Integer> brandIds, List<Integer> categoryIds,
+                                        Double fromPrice, Double toPrice, String search, Pageable pageable);
 }
